fd8f24d1b81fbfc805ab6d6a71e58e2d2086bd3b
Elias Hantula <ehantula@indiana.edu>
2019-05-22 21:04:48 -0400

    fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+var quotes = require('random-movie-quotes')
 app.use(express.static('public'));
 
 app.get('/quote', (req, res) => {
+
   res.json({
     quote: quotes.getQuote()
   });
